0fc4e2979930b17097ebd61399b26fb2ee57442a
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-11-04 19:15:36 +0100

    add test for letting

--- lettings/tests/tests_lettings.py
+++ lettings/tests/tests_lettings.py
@@ -1,2 +1,23 @@
-def test_dummy():
-    assert 1
+import pytest
+
+from django.urls import reverse
+
+from lettings.models import Letting
+
+
+@pytest.mark.django_db
+def test_code_200_lettings_index(client, sample_data):
+    response = client.get(reverse("lettings_index"))
+    assert response.status_code == 200
+    assert "<title>Lettings</title>" in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_code_200_letting(client, sample_data):
+
+    letting = Letting.objects.filter(id=1).first()
+    assert letting is not None, "Letting with id=1 does not exist in the test data."
+
+    response = client.get(reverse("letting", kwargs={"letting_id": letting.id}))
+    assert response.status_code == 200
+    assert f"<title>{letting.title}</title>" in response.content.decode()
