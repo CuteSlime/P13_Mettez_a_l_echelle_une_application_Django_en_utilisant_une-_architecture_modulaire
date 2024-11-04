8ad4462c51b8180c0b5506c4f4651ccbfee07610
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-11-04 19:14:48 +0100

    populate db

--- conftest.py
+++ conftest.py
@@ -1,8 +1,21 @@
 import pytest
 
 from django.test import Client
+from django.contrib.auth.models import User
+
+from lettings.models import Address, Letting
+from profiles.models import Profile
 
 
 @pytest.fixture
 def client():
     return Client()
+
+
+@pytest.fixture
+def sample_data(db):
+    user = User.objects.create(username="test_user", password="testPass")
+    address = Address.objects.create(
+        number="12", street="parla", city="paris", state="France", zip_code="90000", country_iso_code="033",)
+    Letting.objects.create(title="Example 1", address=address)
+    Profile.objects.create(user=user, favorite_city="Grenoble")
