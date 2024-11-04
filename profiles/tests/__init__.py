29d7b7d3bacd93c8d42ed4c3cc722e35f894dffb
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-11-04 19:15:46 +0100

    add test for profiles

--- profiles/tests/__init__.py
+++ profiles/tests/__init__.py
@@ -1,4 +1,4 @@
-from .tests_profile import test_dummy
+from .tests_profile import test_code_200_profiles_index, test_code_200_profile
 
 
-__all__ = ["test_dummy",]
+__all__ = ["test_code_200_profiles_index", "test_code_200_profile"]
